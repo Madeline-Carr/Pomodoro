0f2270ddc240e6db41e219d9f66ef0e16d67a55b
Madeline Carr <Carrm2@vcu.edu>
2018-09-26 21:17:28 -0400

    Finished CSS
    I finished the CSS but it is not fully responsive yet.

---
+++ Script/main.js
@@ -0,0 +1,15 @@
+//VARIABLES
+console.log("Hello Maddie!");
+
+//BUTTON FUNCTIONS
+function startTime(){
+  document.getElementsByClassName("time").innerHTML = "Paragraph changed!";
+};
+
+function stopTime(){
+
+};
+
+function breakTime(){
+
+};
---
+++ main.js
@@ -0,0 +1,19 @@
+//VARIABLES
+console.log("Hello Maddie!");
+/*
+var time3 = document.getElementByClass('time');
+
+//BUTTON FUNCTIONS
+function startTime(){
+  document.getElementByClass("time").innerHTML = "Paragraph changed!";
+}
+
+function stopTime(){
+
+}
+
+function breakTime(){
+
+}
+
+*/
